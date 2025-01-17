c8002e3ecfac8913e54dcc211ddd95c0381cf0a4
aariz6987239 <aarizrasheed2345@gmail.com>
2025-01-17 19:48:58 +0000

    Added subscription form and updated layout

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function detectLocation() {
-    alert("Detecting location...");
-}
-
 function orderNow(item, price) {
-    const name = prompt("Enter your name:");
-    const quantity = prompt(`Enter quantity of ${item}:`);
-    if (name && quantity) {
-        alert(`Order placed for ${quantity} ${item}(s) at ₹${price * quantity}.`);
-    } else {
-        alert("Order canceled.");
-    }
-}
+    alert(`You are ordering ${item} for ${price}.`);
+  }

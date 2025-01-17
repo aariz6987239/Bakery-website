02f248230edb9d0e1a28e59249808eefa43c2d91
aariz6987239 <aarizrasheed2345@gmail.com>
2025-01-17 17:50:15 +0000

    Added gallery slider and layout adjustments

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-// This script is for future interactivity
-console.log("Bakery Delight website loaded successfully!");
+const carouselImages = document.querySelector('.carousel-images');
+const prevButton = document.querySelector('.carousel-prev');
+const nextButton = document.querySelector('.carousel-next');
+
+let index = 0;
+
+prevButton.addEventListener('click', () => {
+  index = (index > 0) ? index - 1 : 4;
+  updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+  index = (index < 4) ? index + 1 : 0;
+  updateCarousel();
+});
+
+function updateCarousel() {
+  const width = carouselImages.clientWidth;
+  carouselImages.style.transform = `translateX(${-index * width}px)`;
+}
+
+carouselImages.addEventListener('click', () => {
+  const imgSrc = carouselImages.children[index].src;
+  window.open(imgSrc, '_blank');
+});

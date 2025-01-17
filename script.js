de0a87620ee0c5067c503b1ae8d893708ba03d03
aariz6987239 <aarizrasheed2345@gmail.com>
2025-01-17 18:07:51 +0000

    Enhanced menu with images and Buy Now buttons

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+// Dynamically populate the form when "Buy Now" is clicked
+function orderItem(itemName, itemPrice) {
+    document.getElementById('item').value = itemName;
+    document.getElementById('price').value = `₹${itemPrice}`;
+    window.location.href = "#order";
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
     console.log("Website loaded and ready!");
   });
